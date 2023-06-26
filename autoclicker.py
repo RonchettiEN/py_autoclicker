785d1f4f3735c7757b19b688e71187cc424cef71
Nico Ronchetti <53882417+RonchettiEN@users.noreply.github.com>
2023-06-26 14:00:24 -0300

    Now have comments in blocks of instructions

--- autoclicker.py
+++ autoclicker.py
@@ -73,17 +73,21 @@ def execute_instructions(instructions, profile_name):
     listener = mouse.Listener(on_click=on_click)
     listener.start()
 
+    clear_screen()
     print(f"Executing profile '{profile_name}'... Press right-click to stop.")
 
     while True:
         for instruction in instructions:
-            x, y, wait_time, repeat = map(int, instruction)
-            for _ in range(repeat):
-                pyautogui.click(x=x, y=y)
-                if wait_time == -1:
-                    pyautogui.sleep(0.001)
-                else:
-                    pyautogui.sleep(wait_time / 1000)
+            if len(instruction) == 4:
+                x, y, wait_time, repeat = map(int, instruction)
+                for _ in range(repeat):
+                    pyautogui.click(x=x, y=y)
+                    if wait_time == -1:
+                        pyautogui.sleep(0.001)
+                    else:
+                        pyautogui.sleep(wait_time / 1000)
+            else:
+                print(f"Comment: {''.join(instruction)}")
 
             if not listener.is_alive():
                 return
@@ -93,19 +97,32 @@ def make_profile():
     clear_screen()
     print("Making profile...")
 
-    coordinates = []
+    initial_comment = input("Enter an initial comment: ")
+
+    coordinates = [initial_comment]
+    capture_events = True  # Variable de control para la captura de eventos
 
     def on_click(x, y, button, pressed):
+        nonlocal capture_events  # Hacer referencia a la variable de control externa
+
         if pressed and button == mouse.Button.left:
-            coordinates.append((x, y))
-            print(f"Recorded coordinates: ({x}, {y})")
+            if capture_events:
+                coordinates.append((x, y))
+                print(f"Recorded coordinates: ({x}, {y})")
 
         if pressed and button == mouse.Button.right:
-            return False  # Stop capturing events
+            capture_events = False  # Detener la captura de eventos
+            comment = input("Enter a comment: ")
+            coordinates.append(comment)
+            print("Comment added.")
+            capture_events = True  # Detener la captura de eventos
+
+        if pressed and button == mouse.Button.middle:
+            return False  # Detener la captura de eventos
 
     with mouse.Listener(on_click=on_click) as listener:
         print("Making profile (click to record coordinates):")
-        print("Press right button to finish.")
+        print("Press right button to add a comment.")
         listener.join()
 
     save_profile(coordinates)
@@ -121,8 +138,11 @@ def save_profile(coordinates):
 
     with open(file_path, "w", newline="") as csv_file:
         csv_writer = csv.writer(csv_file)
-        for x, y in coordinates:
-            csv_writer.writerow([x, y, 1000, 1])
+        for coord in coordinates:
+            if len(coord) == 2:  # Verificar si coord tiene 2 elementos (x, y)
+                csv_writer.writerow([coord[0], coord[1], 1000, 1])
+            else:
+                csv_writer.writerow(coord)
 
     print(f"Profile '{profile_name}' saved successfully.")
 
